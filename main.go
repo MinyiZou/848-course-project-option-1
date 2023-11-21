da0b63bfbf9104a90a79556c185db156dc569b40
Minyi Zou <104998797+MinyiZou@users.noreply.github.com>
2023-11-21 14:15:53 -0800

    Update main.go

--- main.go
+++ main.go
@@ -1,34 +1,6 @@
 package main
-type deck []string
-
-func newDeck() deck {
-	cards := deck{}
-
-	suits := deck{"1", "2", "3", "4"}
-	values := deck{"a", "b", "c"}
-
-	for _, suit := range suits {
-		for _, value := range values {
-			cards = append(cards, suit+" of "+value)
-		}
-	}
-	return cards
-}
-
-func (d deck) print() {
-	for i, card := range d {
-		fmt.Println(i, card)
-	}
-}
-
-func deal(d deck, hanSize int) (deck, deck) {
-	return d[:hanSize], d[hanSize:]
-}
 
 func main() {
-	cards := newDeck()
-
-	hand, remain := deal(cards, 2)
-	hand.print()
-	remain.print()
+	suits := deck{"1", "2", "3", "4"}
+	fmt.Println(suits)
 }
